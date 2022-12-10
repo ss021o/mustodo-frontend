933150afadb9e3d343a6fa631ab83e07bcb7e471
wlsh44 <k87913j@gmail.com>
2022-12-10 19:09:52 +0900

    feat: 할 일 피드 가져오는 기능 추가

--- app/src/main/java/com/cemo/mustodo_test/MainActivity.java
+++ app/src/main/java/com/cemo/mustodo_test/MainActivity.java
@@ -1,35 +1,19 @@
 package com.cemo.mustodo_test;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.cemo.mustodo_test.feed.Frag_feed;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
